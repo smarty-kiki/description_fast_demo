3ac1c7055ad813d1ba8a8bf21fa1a8222bb07438
yaoyang <kiki@smarty.so>
2019-08-16 00:04:05 +0800

    修复启动时初次构造报错的情况

--- shell/start.sh
+++ shell/start.sh
@@ -14,17 +14,17 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'passw
 
 PROJECT_PATH=/var/www/mvc_frame
 
+/usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description
+/bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
+/usr/bin/php /var/www/mvc_frame/public/cli.php migrate:install
+/usr/bin/php /var/www/mvc_frame/public/cli.php migrate
+
 for f in `ls $PROJECT_PATH/domain/description`;do
 
     entity_name=${f%.*}
 
-    /usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description --entity_name=$entity_name
     /usr/bin/php $PROJECT_PATH/public/cli.php crud:make-from-description --entity_name=$entity_name
     /bin/sed -i "/init\ controller/a\include\ CONTROLLER_DIR\.\'\/$entity_name\.php\'\;" $PROJECT_PATH/public/index.php
 done
-
-/bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
-/usr/bin/php /var/www/mvc_frame/public/cli.php migrate:install
-/usr/bin/php /var/www/mvc_frame/public/cli.php migrate
 
 tail -n 100 -f /var/log/nginx/access.log /var/log/nginx/error.log /var/log/php7.0-fpm.log /var/log/mysql/error.log /var/log/redis/redis-server.log /var/log/supervisor/*
